29e27da6e57e1152831d3292be3632a64d90ee6c
SmokeyStack <seansyahril04@gmail.com>
2022-09-28 23:38:00 -0400

    Added Custom Block model

--- include/generator/BlockState.h
+++ include/generator/BlockState.h
@@ -46,4 +46,22 @@ void axisBlock(std::string block, std::string sides, std::string ends) {
     MyFile.close();
 }
 
+void customBlock(std::string block, std::string model, std::string texture) {
+    std::string my_text, temp_text;
+    std::ifstream TempFile("./packs/BP/blocks/" + block + ".json");
+    std::ofstream MyFile("./packs/BP/blocks/" + block + ".json");
+
+    while (getline(TempFile, my_text)) temp_text = temp_text + my_text;
+
+    TempFile.close();
+
+    nlohmann::json j = nlohmann::json::parse(temp_text);
+    j["minecraft:block"]["components"]["minecraft:geometry"] = model;
+    j["minecraft:block"]["components"]["material_instances"]["*"]["texture"] =
+        texture;
+
+    MyFile << j.dump(4);
+    MyFile.close();
+}
+
 #endif
