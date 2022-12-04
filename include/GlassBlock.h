0cd13e3ae87881a5e78068cd6fa78d1a7c824041
SmokeyStack <seansyahril04@gmail.com>
2022-12-04 17:47:59 -0500

    Added Glass class

---
+++ include/GlassBlock.h
@@ -0,0 +1,26 @@
+#ifndef GLASSBLOCK_H
+#define GLASSBLOCK_H
+
+#include "Block.h"
+#include "BlockProperty.h"
+#include "json.hpp"
+
+class GlassBlock : public Block {
+   public:
+    GlassBlock(BlockProperty::Property property) {
+        block_light_filter = property.block_light_filter;
+        breathability = property.breathability;
+        friction = property.friction;
+        light_emission = property.light_emission;
+        rotation = property.rotation;
+    };
+
+    json output(std::string mod_id, std::string id) {
+        j = Block::output(mod_id, id);
+        j["minecraft:block"]["components"]["minecraft:block_light_filter"] = 0;
+
+        return j;
+    }
+};
+
+#endif
