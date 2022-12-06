2809e4c4cd7c49eba082845facd87cd3fd6b63fe
SmokeyStack <seansyahril04@gmail.com>
2022-12-06 08:31:24 -0500

    Added BushBlock class

---
+++ include/BushBlock.h
@@ -0,0 +1,31 @@
+#ifndef BUSHBLOCK_H
+#define BUSHBLOCK_H
+
+#include <iostream>
+
+#include "Block.h"
+#include "BlockProperty.h"
+#include "json.hpp"
+
+class BushBlock : public Block {
+   public:
+    BushBlock(BlockProperty::Property property) {
+        block_light_filter = property.block_light_filter;
+        friction = property.friction;
+        light_emission = property.light_emission;
+        rotation = property.rotation;
+    }
+
+    json output(std::string mod_id, std::string id) {
+        j = Block::output(mod_id, id);
+        j["minecraft:block"]["components"]["minecraft:placement_filter"]
+         ["conditions"] = {{{"allowed_faces", json::array({"up"})},
+                            {"block_filter",
+                             json::array({{{"tags", "q.any_tag('dirt')"}},
+                                          {{"tags", "q.any_tag('grass')"}}})}}};
+
+        return j;
+    }
+};
+
+#endif
