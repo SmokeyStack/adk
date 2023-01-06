828dda2b8fde695e3fbfd2f38f5b637b9ca45dc3
SmokeyStack <seansyahril04@gmail.com>
2023-01-05 22:03:19 -0500

    Added more properties

--- include/Block.h
+++ include/Block.h
@@ -9,8 +9,15 @@
 class Block {
    protected:
     int block_light_filter;
+    nlohmann::json::object_t crafting;
+    nlohmann::json::object_t explosion;
+    nlohmann::json::object_t mining;
+    std::string display_name;
+    nlohmann::json::object_t flammable;
     double friction;
     int light_emission;
+    std::string loot;
+    std::string colour;
     std::vector<int> rotation;
 
    public:
@@ -21,8 +28,15 @@ class Block {
     Block(){};
     Block(BlockProperty::Property property) {
         block_light_filter = property.block_light_filter;
+        crafting = property.crafting;
+        explosion = property.explosion;
+        mining = property.mining;
+        display_name = property.display_name;
+        flammable = property.flammable;
         friction = property.friction;
         light_emission = property.light_emission;
+        loot = property.loot;
+        colour = property.colour;
         rotation = property.rotation;
     }
 
@@ -36,12 +50,39 @@ class Block {
             j["minecraft:block"]["components"]["minecraft:light_dampening"] =
                 block_light_filter;
 
+        if (crafting != json::object_t({{"crafting_table", {"crafting_table"}},
+                                        {"table_name", "Foo Bar"}}))
+            j["minecraft:block"]["components"]["minecraft:crafting_table"] =
+                crafting;
+        if (explosion != json::object_t({{"explosion_resistance", 0}}))
+            j["minecraft:block"]["components"]
+             ["minecraft:destructible_by_explosion"] = explosion;
+
+        if (mining != json::object_t({{"seconds_to_destory", 0.0}}))
+            j["minecraft:block"]["components"]
+             ["minecraft:destructible_by_mining"] = mining;
+
+        if (display_name != "")
+            j["minecraft:block"]["components"]["minecraft:display_name"] =
+                display_name;
+
+        if (flammable != json::object_t({{"catch_chance_modifier", 5},
+                                         {"destroy_chance_modifier", 20}}))
+            j["minecraft:block"]["components"]["minecraft:flammable"] =
+                flammable;
+
         if (friction != 0.4)
             j["minecraft:block"]["components"]["minecraft:friction"] = friction;
 
         if (light_emission != 0)
             j["minecraft:block"]["components"]["minecraft:light_emission"] =
                 light_emission;
+
+        if (loot != "")
+            j["minecraft:block"]["components"]["minecraft:loot"] = loot;
+
+        if (colour != "")
+            j["minecraft:block"]["components"]["minecraft:map_color"] = colour;
 
         if (rotation != std::vector<int>{0, 0, 0})
             j["minecraft:block"]["components"]["minecraft:rotation"] = rotation;
--- include/BlockProperty.h
+++ include/BlockProperty.h
@@ -4,17 +4,55 @@
 #include <string>
 #include <vector>
 
+#include "json.hpp"
+
 class BlockProperty {
    public:
     class Property {
        public:
         int block_light_filter = 15;
+        nlohmann::json::object_t crafting = {
+            {"crafting_table", {"crafting_table"}}, {"table_name", "Foo Bar"}};
+        nlohmann::json::object_t explosion = {{"explosion_resistance", 0}};
+        nlohmann::json::object_t mining = {{"seconds_to_destory", 0.0}};
+        std::string display_name = "";
+        nlohmann::json::object_t flammable = {{"catch_chance_modifier", 5},
+                                              {"destroy_chance_modifier", 20}};
         double friction = 0.4;
+        std::string geometry = "";
         int light_emission = 0;
+        std::string loot = "";
+        std::string colour = "";
         std::vector<int> rotation = {0, 0, 0};
 
         Property setBlockLightFilter(int blf) {
             this->block_light_filter = blf;
+            return *this;
+        }
+
+        Property setCrafting(std::vector<std::string>& tags, std::string name) {
+            this->crafting = {{"crafting_table", tags}, {"table_name", name}};
+            return *this;
+        }
+
+        Property setExplosion(float e) {
+            this->explosion = {{"explosion_resistance", e}};
+            return *this;
+        }
+
+        Property setMining(float m) {
+            this->mining = {{"seconds_to_destory", m}};
+            return *this;
+        }
+
+        Property setName(std::string n) {
+            this->display_name = n;
+            return *this;
+        }
+
+        Property setFlammable(int catch_chance, int destroy) {
+            this->flammable = {{"catch_chance_modifier", catch_chance},
+                               {"destroy_chance_modifier", destroy}};
             return *this;
         }
 
@@ -23,8 +61,23 @@ class BlockProperty {
             return *this;
         }
 
+        Property setGeometry(std::string g) {
+            this->geometry = g;
+            return *this;
+        }
+
         Property setLightEmission(int le) {
             this->light_emission = le;
+            return *this;
+        }
+
+        Property setLoot(std::string l) {
+            this->loot = l;
+            return *this;
+        }
+
+        Property setColour(std::string c) {
+            this->colour = c;
             return *this;
         }
 
