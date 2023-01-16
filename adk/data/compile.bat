4e586eff1c64740f26e7eab11128431b64f2d98c
SmokeyStack <seansyahril04@gmail.com>
2023-01-16 12:19:49 -0500

    Fixed compiling errors

--- adk/data/compile.bat
+++ adk/data/compile.bat
@@ -1,4 +1,4 @@
-cmake -S ./data -B adk-build -DCMAKE_BUILD_TYPE=Release
+cmake -S ./data/adk -B adk-build -DCMAKE_BUILD_TYPE=Release
 cmake --build adk-build --config Release --parallel
 .\adk-build\Release\adk.exe
 @REM .\adk-build\Release\datagen.exe
