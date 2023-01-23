adeb6f47b8febfa075ca6367065aa70049d455c7
SmokeyStack <seansyahril04@gmail.com>
2023-01-23 18:37:22 -0500

    Turned damage_chance from float to int - Microsoft Docs said it was a float - Content Log and HTML Docs said it was an object

--- adk/data/include/Armor.h
+++ adk/data/include/Armor.h
@@ -31,23 +31,37 @@ class Armor : public Item {
     int _protection;
     std::string _slot;
     int _durability;
+    bool _dispensable;
+    int _damage_chance_min;
+    int _damage_chance_max;
 
    public:
     Armor(ItemProperty::Property property, int protection, ArmorSlot test,
-          int durability) {
+          int durability, bool dispensable, int damage_chance_min,
+          int damage_chance_max) {
         display_name = property.display_name;
         stack = property.stack;
         _protection = protection;
         _slot = getArmorSlot(test);
         _durability = durability;
+        _dispensable = dispensable;
+        _damage_chance_min = damage_chance_min;
+        _damage_chance_max = damage_chance_max;
     }
+
     json output(std::string mod_id, std::string id) {
         j = Item::output(mod_id, id);
         j["minecraft:item"]["components"]["minecraft:armor"]["protection"] =
             _protection;
         j["minecraft:item"]["components"]["minecraft:wearable"]["slot"] = _slot;
+        j["minecraft:item"]["components"]["minecraft:wearable"]["dispensable"] =
+            _dispensable;
         j["minecraft:item"]["components"]["minecraft:durability"]
          ["max_durability"] = _durability;
+        j["minecraft:item"]["components"]["minecraft:durability"]
+         ["damage_chance"]["min"] = _damage_chance_min;
+        j["minecraft:item"]["components"]["minecraft:durability"]
+         ["damage_chance"]["max"] = _damage_chance_max;
 
         return j;
     }
