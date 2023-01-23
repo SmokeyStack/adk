744fb6833a170451b3eb9d4942376202b63f8ed8
SmokeyStack <seansyahril04@gmail.com>
2023-01-23 18:40:44 -0500

    Added Head Class

---
+++ adk/data/include/Head.h
@@ -0,0 +1,86 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include "Block.h"
+
+class Head : public Block {
+   public:
+    Head(BlockProperty::Property property) {
+        block_light_filter = property.block_light_filter;
+        friction = property.friction;
+        light_emission = property.light_emission;
+        rotation = property.rotation;
+    }
+
+    json output(std::string mod_id, std::string id) {
+        j = Block::output(mod_id, id);
+        j["minecraft:block"]["description"]["properties"][mod_id +
+                                                          ":rotation"] =
+            json::array({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+        j["minecraft:block"]["components"]["minecraft:on_player_placing"]
+         ["event"] = mod_id + ":set_rotation";
+        j["minecraft:block"]["events"][mod_id + ":set_rotation"]
+         ["set_block_property"][mod_id + ":rotation"] =
+             "(q.body_y_rotation > -11.25 && q.body_y_rotation < 11.25) ? 0 : "
+             "(q.body_y_rotation > 11.25 && q.body_y_rotation < 33.75) ? 1 : "
+             "(q.body_y_rotation > 33.75 && q.body_y_rotation < 56.25) ? 2 : "
+             "(q.body_y_rotation > 56.25 && q.body_y_rotation < 78.25) ? 3 : "
+             "(q.body_y_rotation > 78.25 && q.body_y_rotation < 101.25) ? 4 : "
+             "(q.body_y_rotation > 101.25 && q.body_y_rotation < 123.75) ? 5 : "
+             "(q.body_y_rotation > 123.75 && q.body_y_rotation < 146.25) ? 6 : "
+             "(q.body_y_rotation > 146.25 && q.body_y_rotation < 168.75) ? 7 : "
+             "(q.body_y_rotation > 168.75 || q.body_y_rotation < -168.75) ? 8 "
+             ": (q.body_y_rotation > -168.75 && q.body_y_rotation < -146.25) ? "
+             "9 : (q.body_y_rotation > -146.25 && q.body_y_rotation < -123.75) "
+             "? 10 : (q.body_y_rotation > -123.75 && q.body_y_rotation < "
+             "-101.25) ? 11 : (q.body_y_rotation > -101.25 && "
+             "q.body_y_rotation < -78.25) ? 12 : (q.body_y_rotation > -78.25 "
+             "&& q.body_y_rotation < -56.25) ? 13 : (q.body_y_rotation > "
+             "-56.25 && q.body_y_rotation < -33.75) ? 14 : (q.body_y_rotation "
+             "> -33.75 && q.body_y_rotation < -11.25) ? 15";
+        j["minecraft:block"]["permutations"] = {
+            {{"components", {{"minecraft:geometry", "geometry.head_n"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 0"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_nne"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 1"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_ne"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 2"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_ene"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 3"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_e"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 4"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_ese"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 5"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_se"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 6"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_sse"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 7"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_s"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 8"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_ssw"}}},
+             {"condition", "q.block_property('" + mod_id + ":rotation') == 9"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_sw"}}},
+             {"condition",
+              "q.block_property('" + mod_id + ":rotation') == 10"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_wsw"}}},
+             {"condition",
+              "q.block_property('" + mod_id + ":rotation') == 11"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_w"}}},
+             {"condition",
+              "q.block_property('" + mod_id + ":rotation') == 12"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_wnw"}}},
+             {"condition",
+              "q.block_property('" + mod_id + ":rotation') == 13"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_nw"}}},
+             {"condition",
+              "q.block_property('" + mod_id + ":rotation') == 14"}},
+            {{"components", {{"minecraft:geometry", "geometry.head_nnw"}}},
+             {"condition",
+              "q.block_property('" + mod_id + ":rotation') == 15"}}};
+        j["minecraft:block"]["components"].erase("minecraft:unit_cube");
+
+        return j;
+    };
+};
+
+#endif
--- adk/data/include/generator/BlockState.h
+++ adk/data/include/generator/BlockState.h
@@ -1,6 +1,7 @@
 #ifndef BLOCKSTATE_H
 #define BLOCKSTATE_H
 
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -8,23 +9,25 @@
 
 #include "json.hpp"
 
+namespace fs = std::filesystem;
+
 void simpleBlock(std::string block) {
     std::string my_text, temp_text;
-    std::ifstream TempFile("./packs/BP/blocks/" + block + ".json");
+    std::ifstream TempFile("./BP/blocks/" + block + ".json");
     nlohmann::json j = nlohmann::json::parse(TempFile);
 
     j["minecraft:block"]["components"]["material_instances"]["*"]["texture"] =
         block;
 
     TempFile.close();
-    std::ofstream MyFile("./packs/BP/blocks/" + block + ".json");
+    std::ofstream MyFile("./BP/blocks/" + block + ".json");
     MyFile << j.dump(4);
     MyFile.close();
 }
 
 void axisBlock(std::string block, std::string sides, std::string ends) {
     std::string my_text, temp_text;
-    std::ifstream TempFile("./packs/BP/blocks/" + block + ".json");
+    std::ifstream TempFile("./BP/blocks/" + block + ".json");
     nlohmann::json j = nlohmann::json::parse(TempFile);
 
     j["minecraft:block"]["components"]["material_instances"]["*"]["texture"] =
@@ -37,14 +40,14 @@ void axisBlock(std::string block, std::string sides, std::string ends) {
      ["texture"] = "ends";
 
     TempFile.close();
-    std::ofstream MyFile("./packs/BP/blocks/" + block + ".json");
+    std::ofstream MyFile("./BP/blocks/" + block + ".json");
     MyFile << j.dump(4);
     MyFile.close();
 }
 
 void customBlock(std::string block, std::string model, std::string texture) {
     std::string my_text, temp_text;
-    std::ifstream TempFile("./packs/BP/blocks/" + block + ".json");
+    std::ifstream TempFile("./BP/blocks/" + block + ".json");
     nlohmann::json j = nlohmann::json::parse(TempFile);
 
     j["minecraft:block"]["components"]["minecraft:geometry"] = model;
@@ -52,14 +55,14 @@ void customBlock(std::string block, std::string model, std::string texture) {
         texture;
 
     TempFile.close();
-    std::ofstream MyFile("./packs/BP/blocks/" + block + ".json");
+    std::ofstream MyFile("./BP/blocks/" + block + ".json");
     MyFile << j.dump(4);
     MyFile.close();
 }
 
 void tintedGlass(std::string block, std::string texture) {
     std::string my_text, temp_text;
-    std::ifstream TempFile("./packs/BP/blocks/" + block + ".json");
+    std::ifstream TempFile("./BP/blocks/" + block + ".json");
     nlohmann::json j = nlohmann::json::parse(TempFile);
 
     j["minecraft:block"]["components"]["minecraft:material_instances"]["*"]
@@ -68,9 +71,44 @@ void tintedGlass(std::string block, std::string texture) {
      ["render_method"] = "blend";
 
     TempFile.close();
-    std::ofstream MyFile("./packs/BP/blocks/" + block + ".json");
+    std::ofstream MyFile("./BP/blocks/" + block + ".json");
     MyFile << j.dump(4);
     MyFile.close();
+}
+
+void head(std::string block, std::string north, std::string east,
+          std::string south, std::string west, std::string up,
+          std::string down) {
+    std::string my_text, temp_text;
+    std::ifstream TempFile("./BP/blocks/" + block + ".json");
+    nlohmann::json j = nlohmann::json::parse(TempFile);
+
+    j["minecraft:block"]["components"]["minecraft:material_instances"]["north"]
+     ["texture"] = north;
+    j["minecraft:block"]["components"]["minecraft:material_instances"]["east"]
+     ["texture"] = east;
+    j["minecraft:block"]["components"]["minecraft:material_instances"]["south"]
+     ["texture"] = south;
+    j["minecraft:block"]["components"]["minecraft:material_instances"]["west"]
+     ["texture"] = west;
+    j["minecraft:block"]["components"]["minecraft:material_instances"]["up"]
+     ["texture"] = up;
+    j["minecraft:block"]["components"]["minecraft:material_instances"]["down"]
+     ["texture"] = down;
+
+    TempFile.close();
+    std::ofstream MyFile("./BP/blocks/" + block + ".json");
+    MyFile << j.dump(4);
+    MyFile.close();
+
+    if (!fs::exists("./RP/models/entity/head.geo.json")) {
+        if (!fs::exists("./RP/models/entity")) {
+            fs::create_directory("./RP/models/");
+            fs::create_directory("./RP/models/entity/");
+        }
+
+        fs::copy("./data/adk/assets/head.geo.json", "./RP/models/entity");
+    }
 }
 
 #endif
