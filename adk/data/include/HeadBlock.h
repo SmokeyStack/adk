20e85da0f35a9ed9f7f3690691c7706d0ea767f9
SmokeyStack <seansyahril04@gmail.com>
2023-01-31 16:50:52 -0500

    Implemented wall feature for HeadBlock

--- adk/data/include/HeadBlock.h
+++ adk/data/include/HeadBlock.h
@@ -16,9 +16,17 @@ class Head : public Block {
 
     json output(std::string mod_id, std::string id) {
         j = Block::output(mod_id, id);
+
+        // Properties
+        j["minecraft:block"]["description"]["properties"][mod_id + ":is_wall"] =
+            json::array({false, true});
+        j["minecraft:block"]["description"]["properties"]
+         [mod_id + ":placement"] = json::array({0, 1, 2, 3, 4, 5});
         j["minecraft:block"]["description"]["properties"][mod_id +
                                                           ":rotation"] =
             json::array({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+
+        // Components
         j["minecraft:block"]["components"]["minecraft:collision_box"]
          ["origin"] = {-4, 0, -4};
         j["minecraft:block"]["components"]["minecraft:collision_box"]["size"] =
@@ -29,64 +37,124 @@ class Head : public Block {
             {8, 8, 8};
         j["minecraft:block"]["components"]["minecraft:on_player_placing"]
          ["event"] = mod_id + ":set_rotation";
+        j["minecraft:block"]["components"]["minecraft:geometry"] =
+            "geometry.head_0";
+
+        // Events
+        j["minecraft:block"]["events"][mod_id + ":set_rotation"]
+         ["set_block_property"][mod_id + ":is_wall"] =
+             "(q.block_face > 1) ? true : false";
+        j["minecraft:block"]["events"][mod_id + ":set_rotation"]
+         ["set_block_property"][mod_id + ":placement"] = "q.block_face";
         j["minecraft:block"]["events"][mod_id + ":set_rotation"]
          ["set_block_property"][mod_id + ":rotation"] =
-             "(q.body_y_rotation > -11.25 && q.body_y_rotation < 11.25) ? 0 : "
-             "(q.body_y_rotation > 11.25 && q.body_y_rotation < 33.75) ? 1 : "
-             "(q.body_y_rotation > 33.75 && q.body_y_rotation < 56.25) ? 2 : "
-             "(q.body_y_rotation > 56.25 && q.body_y_rotation < 78.25) ? 3 : "
-             "(q.body_y_rotation > 78.25 && q.body_y_rotation < 101.25) ? 4 : "
-             "(q.body_y_rotation > 101.25 && q.body_y_rotation < 123.75) ? 5 : "
-             "(q.body_y_rotation > 123.75 && q.body_y_rotation < 146.25) ? 6 : "
-             "(q.body_y_rotation > 146.25 && q.body_y_rotation < 168.75) ? 7 : "
-             "(q.body_y_rotation > 168.75 || q.body_y_rotation < -168.75) ? 8 "
-             ": (q.body_y_rotation > -168.75 && q.body_y_rotation < -146.25) ? "
-             "9 : (q.body_y_rotation > -146.25 && q.body_y_rotation < -123.75) "
+             "(q.body_y_rotation > -11.25 && q.body_y_rotation < 11.25) ? 0 "
+             ": "
+             "(q.body_y_rotation > 11.25 && q.body_y_rotation < 33.75) ? 1 "
+             ": "
+             "(q.body_y_rotation > 33.75 && q.body_y_rotation < 56.25) ? 2 "
+             ": "
+             "(q.body_y_rotation > 56.25 && q.body_y_rotation < 78.25) ? 3 "
+             ": "
+             "(q.body_y_rotation > 78.25 && q.body_y_rotation < 101.25) ? 4 "
+             ": "
+             "(q.body_y_rotation > 101.25 && q.body_y_rotation < 123.75) ? "
+             "5 : "
+             "(q.body_y_rotation > 123.75 && q.body_y_rotation < 146.25) ? "
+             "6 : "
+             "(q.body_y_rotation > 146.25 && q.body_y_rotation < 168.75) ? "
+             "7 : "
+             "(q.body_y_rotation > 168.75 || q.body_y_rotation < -168.75) ? "
+             "8 "
+             ": (q.body_y_rotation > -168.75 && q.body_y_rotation < "
+             "-146.25) ? "
+             "9 : (q.body_y_rotation > -146.25 && q.body_y_rotation < "
+             "-123.75) "
              "? 10 : (q.body_y_rotation > -123.75 && q.body_y_rotation < "
              "-101.25) ? 11 : (q.body_y_rotation > -101.25 && "
-             "q.body_y_rotation < -78.25) ? 12 : (q.body_y_rotation > -78.25 "
+             "q.body_y_rotation < -78.25) ? 12 : (q.body_y_rotation > "
+             "-78.25 "
              "&& q.body_y_rotation < -56.25) ? 13 : (q.body_y_rotation > "
-             "-56.25 && q.body_y_rotation < -33.75) ? 14 : (q.body_y_rotation "
+             "-56.25 && q.body_y_rotation < -33.75) ? 14 : "
+             "(q.body_y_rotation "
              "> -33.75 && q.body_y_rotation < -11.25) ? 15";
-        j["minecraft:block"]["permutations"] = {
-            {{"components", {{"minecraft:geometry", "geometry.head_n"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 0"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_nne"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 1"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_ne"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 2"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_ene"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 3"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_e"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 4"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_ese"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 5"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_se"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 6"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_sse"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 7"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_s"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 8"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_ssw"}}},
-             {"condition", "q.block_property('" + mod_id + ":rotation') == 9"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_sw"}}},
-             {"condition",
-              "q.block_property('" + mod_id + ":rotation') == 10"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_wsw"}}},
-             {"condition",
-              "q.block_property('" + mod_id + ":rotation') == 11"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_w"}}},
-             {"condition",
-              "q.block_property('" + mod_id + ":rotation') == 12"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_wnw"}}},
-             {"condition",
-              "q.block_property('" + mod_id + ":rotation') == 13"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_nw"}}},
-             {"condition",
-              "q.block_property('" + mod_id + ":rotation') == 14"}},
-            {{"components", {{"minecraft:geometry", "geometry.head_nnw"}}},
-             {"condition",
-              "q.block_property('" + mod_id + ":rotation') == 15"}}};
+
+        // Permutations
+        j["minecraft:block"]["permutations"].push_back(
+            {{"components",
+              {{"minecraft:collision_box",
+                {{"origin", {-4, 4, 0}}, {"size", {8, 8, 8}}}},
+               {"minecraft:selection_box",
+                {{"origin", {-4, 4, 0}}, {"size", {8, 8, 8}}}},
+               {"minecraft:geometry", "geometry.head_wall"}}},
+             {"condition", "q.block_property('smokey_bedrock:is_wall')"}});
+
+        j["minecraft:block"]["permutations"].push_back(
+            {{"components", {{"minecraft:rotation", {0, 0, 0}}}},
+             {"condition", "q.block_property('" + mod_id +
+                               ":placement') == " + std::to_string(2)}});
+        j["minecraft:block"]["permutations"].push_back(
+            {{"components", {{"minecraft:rotation", {0, 180, 0}}}},
+             {"condition", "q.block_property('" + mod_id +
+                               ":placement') == " + std::to_string(3)}});
+        j["minecraft:block"]["permutations"].push_back(
+            {{"components", {{"minecraft:rotation", {0, 90, 0}}}},
+             {"condition", "q.block_property('" + mod_id +
+                               ":placement') == " + std::to_string(4)}});
+        j["minecraft:block"]["permutations"].push_back(
+            {{"components", {{"minecraft:rotation", {0, 270, 0}}}},
+             {"condition", "q.block_property('" + mod_id +
+                               ":placement') == " + std::to_string(5)}});
+
+        for (int a = 0; a < 16; a += 4) {
+            j["minecraft:block"]["permutations"].push_back(
+                {{"components", {{"minecraft:rotation", {0, (a / 4) * 90, 0}}}},
+                 {"condition", "q.block_property('" + mod_id +
+                                   ":rotation') == " + std::to_string(a) +
+                                   " && (q.block_property('" + mod_id +
+                                   ":placement') == 0 "
+                                   "|| q.block_property('" +
+                                   mod_id +
+                                   ":placement') == "
+                                   "1)"}});
+            j["minecraft:block"]["permutations"].push_back(
+                {{"components",
+                  {{"minecraft:geometry", "geometry.head_22_5"},
+                   {"minecraft:rotation", {0, (a / 4) * 90, 0}}}},
+                 {"condition", "q.block_property('" + mod_id +
+                                   ":rotation') == " + std::to_string(a + 1) +
+                                   " && (q.block_property('" + mod_id +
+                                   ":placement') == 0 "
+                                   "|| q.block_property('" +
+                                   mod_id +
+                                   ":placement') == "
+                                   "1)"}});
+            j["minecraft:block"]["permutations"].push_back(
+                {{"components",
+                  {{"minecraft:geometry", "geometry.head_45"},
+                   {"minecraft:rotation", {0, (a / 4) * 90, 0}}}},
+                 {"condition", "q.block_property('" + mod_id +
+                                   ":rotation') == " + std::to_string(a + 2) +
+                                   " && (q.block_property('" + mod_id +
+                                   ":placement') == 0 "
+                                   "|| q.block_property('" +
+                                   mod_id +
+                                   ":placement') == "
+                                   "1)"}});
+            j["minecraft:block"]["permutations"].push_back(
+                {{"components",
+                  {{"minecraft:geometry", "geometry.head_67_5"},
+                   {"minecraft:rotation", {0, (a / 4) * 90, 0}}}},
+                 {"condition", "q.block_property('" + mod_id +
+                                   ":rotation') == " + std::to_string(a + 3) +
+                                   " && (q.block_property('" + mod_id +
+                                   ":placement') == 0 "
+                                   "|| q.block_property('" +
+                                   mod_id +
+                                   ":placement') == "
+                                   "1)"}});
+        }
+
         j["minecraft:block"]["components"].erase("minecraft:unit_cube");
 
         return j;
