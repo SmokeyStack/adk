ba395a3614bb2b63accca7160828ad1c550f226c
SmokeyStack <seansyahril04@gmail.com>
2023-02-25 23:26:39 -0500

    Added more item properties

--- adk/data/include/Item.h
+++ adk/data/include/Item.h
@@ -9,7 +9,18 @@
 class Item {
    protected:
     std::string _display_name;
+    std::string _icon;
     int _stack;
+    std::string _block_placer;
+    std::vector<std::string> _block_placer_placement;
+    std::string _cooldown_category;
+    float _cooldown_time;
+    std::string _dye;
+    std::string _entity_placer;
+    std::vector<std::string> _entity_placer_placement;
+    std::vector<std::string> _entity_placer_dispense;
+    std::vector<float> _offset_main;
+    std::vector<float> _offset_offhand;
 
    public:
     using json = nlohmann::json;
@@ -21,7 +32,18 @@ class Item {
     /// @param property An ItemProeprty object
     Item(ItemProperty::Property property) {
         _display_name = property.display_name;
+        _icon = property.icon;
         _stack = property.stack;
+        _block_placer = property.block_placer;
+        _block_placer_placement = property.block_placer_placement;
+        _cooldown_category = property.cooldown_category;
+        _cooldown_time = property.cooldown_time;
+        _dye = property.dye;
+        _entity_placer = property.entity_placer;
+        _entity_placer_placement = property.entity_placer_placement;
+        _entity_placer_dispense = property.entity_placer_dispense;
+        _offset_main = property.offset_main;
+        _offset_offhand = property.offset_offhand;
     }
 
     /// @brief Generates the json object
@@ -30,10 +52,60 @@ class Item {
     /// @return json object
     virtual json output(std::string mod_id, std::string id) {
         j["format_version"] = "1.19.50";
-        j["minecraft:item"]["components"]["minecraft:display_name"]["value"] =
-            _display_name;
-        j["minecraft:item"]["components"]["minecraft:max_stack_size"] = _stack;
         j["minecraft:item"]["description"]["identifier"] = mod_id + ":" + id;
+
+        if (!_display_name.empty())
+            j["minecraft:item"]["components"]["minecraft:display_name"]
+             ["value"] = _display_name;
+
+        if (!_icon.empty())
+            j["minecraft:item"]["components"]["minecraft:icon"]["texture"] =
+                _icon;
+
+        if (_stack != 64 && _stack != NULL)
+            j["minecraft:item"]["components"]["minecraft:max_stack_size"] =
+                _stack;
+
+        if (!_block_placer.empty()) {
+            j["minecraft:item"]["components"]["minecraft:block_placer"]
+             ["block"] = _block_placer;
+
+            if (!_block_placer_placement.empty())
+                j["minecraft:item"]["components"]["minecraft:block_placer"]
+                 ["use_on"] = _block_placer_placement;
+        }
+
+        if (!_cooldown_category.empty() && _cooldown_time != NULL) {
+            j["minecraft:item"]["components"]["minecraft:cooldown"]
+             ["category"] = _cooldown_category;
+            j["minecraft:item"]["components"]["minecraft:cooldown"]
+             ["duration"] = _cooldown_time;
+        }
+
+        if (!_dye.empty())
+            j["minecraft:item"]["components"]["minecraft:dye_powder"]["color"] =
+                _dye;
+
+        if (!_entity_placer.empty()) {
+            j["minecraft:item"]["components"]["minecraft:block_placer"]
+             ["entity"] = _entity_placer;
+
+            if (!_entity_placer_placement.empty())
+                j["minecraft:item"]["components"]["minecraft:block_placer"]
+                 ["use_on"] = _entity_placer_placement;
+
+            if (!_entity_placer_dispense.empty())
+                j["minecraft:item"]["components"]["minecraft:block_placer"]
+                 ["dispense_on"] = _entity_placer_dispense;
+        }
+
+        if (!_offset_main.empty())
+            j["minecraft:item"]["components"]["minecraft:render_offsets"]
+             ["main_hand"] = _offset_main;
+
+        if (!_offset_offhand.empty())
+            j["minecraft:item"]["components"]["minecraft:render_offsets"]
+             ["off_hand"] = _offset_offhand;
 
         return j;
     }
--- adk/data/include/ItemProperty.h
+++ adk/data/include/ItemProperty.h
@@ -3,13 +3,81 @@
 
 #include <string>
 
+enum class Dye {
+    BLACK,
+    RED,
+    GREEN,
+    BROWN,
+    BLUE,
+    PURPLE,
+    CYAN,
+    SILVER,
+    GRAY,
+    PINK,
+    LIME,
+    YELLOW,
+    LIGHTBLUE,
+    MAGENTA,
+    ORANGE,
+    WHITE
+};
+
+std::string getDye(Dye dye) {
+    switch (dye) {
+        case Dye::BLACK:
+            return "black";
+        case Dye::RED:
+            return "red";
+        case Dye::GREEN:
+            return "green";
+        case Dye::BROWN:
+            return "brown";
+        case Dye::BLUE:
+            return "blue";
+        case Dye::PURPLE:
+            return "purple";
+        case Dye::CYAN:
+            return "cyan";
+        case Dye::SILVER:
+            return "silver";
+        case Dye::GRAY:
+            return "gray";
+        case Dye::PINK:
+            return "pink";
+        case Dye::LIME:
+            return "lime";
+        case Dye::YELLOW:
+            return "yellow";
+        case Dye::LIGHTBLUE:
+            return "lightblue";
+        case Dye::MAGENTA:
+            return "magenta";
+        case Dye::ORANGE:
+            return "orange";
+        case Dye::WHITE:
+            return "white";
+        default:
+            return "Error";
+    }
+}
+
 class ItemProperty {
    public:
     class Property {
        public:
-        std::string display_name = "";
-        std::string icon = "";
-        int stack = 64;
+        std::string display_name;
+        std::string icon;
+        int stack;
+        std::string block_placer;
+        std::vector<std::string> block_placer_placement;
+        std::string cooldown_category;
+        float cooldown_time;
+        std::string dye;
+        std::string entity_placer;
+        std::vector<std::string> entity_placer_placement;
+        std::vector<std::string> entity_placer_dispense;
+        std::vector<float> offset_main;
+        std::vector<float> offset_offhand;
 
         /// @brief Sets "display_name" component
         /// @param n Display Name item component. Display Names set the name to
@@ -31,6 +99,43 @@ class ItemProperty {
         /// @param s The max stack size this item can hold
         Property setStack(int s) {
             this->stack = s;
+            return *this;
+        }
+
+        Property setBlockPlacer(std::string block,
+                                std::vector<std::string> placement = {}) {
+            this->block_placer = block;
+            this->block_placer_placement = placement;
+            return *this;
+        }
+
+        Property setCooldown(std::string category, float time) {
+            this->cooldown_category = category;
+            this->cooldown_time = time;
+            return *this;
+        }
+
+        Property setDye(Dye dye) {
+            this->dye = getDye(dye);
+            return *this;
+        }
+
+        Property setEntityPlacer(std::string entity,
+                                 std::vector<std::string> placement = {},
+                                 std::vector<std::string> dispense = {}) {
+            this->entity_placer = entity;
+            this->entity_placer_placement = placement;
+            this->entity_placer_dispense = dispense;
+            return *this;
+        }
+
+        Property setRenderMain(std::vector<float> offset) {
+            this->offset_main = offset;
+            return *this;
+        }
+
+        Property setRenderOffhand(std::vector<float> offset) {
+            this->offset_offhand = offset;
             return *this;
         }
     };
