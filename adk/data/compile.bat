078d5cca32c67cf5843dab1e710380212a7573f9
SmokeyStack <seansyahril04@gmail.com>
2023-11-21 01:17:54 -0500

    Updated compile args

--- adk/data/compile.bat
+++ adk/data/compile.bat
@@ -1,3 +1,3 @@
-cmake -S ./data/adk -B adk-build -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=%1
+cmake -S ./data/adk -B adk-build -DCMAKE_BUILD_TYPE=Release
 cmake --build adk-build --config Release --parallel
 .\adk-build\Release\adk.exe
