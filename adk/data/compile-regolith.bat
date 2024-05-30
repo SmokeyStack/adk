4da432f5ef4bf1e524940bcc9f3c58b198363fac
SmokeyStack <seansyahril04@gmail.com>
2024-05-29 21:58:16 -0400

    Finally fixed compile-regolith.bat

--- adk/data/compile-regolith.bat
+++ adk/data/compile-regolith.bat
@@ -1,7 +1,5 @@
-IF "%~2"==""(
+IF [%2]==[] (
     cmake -S ./data/adk -B adk-build -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=%1
-) ELSE (
-    cmake -S ./data/adk -B adk-build -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DGITHUB_ACTION=%2
-)
+) ELSE (cmake -S ./data/adk -B adk-build -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DGITHUB_ACTION=%2)
 cmake --build adk-build --config Release --parallel
-./adk-build/Release/adk.exe
+.\adk-build\Release\adk.exe
